af98e2a06bb8f5f93c7bb6b8e3b3cc32e3cb1caf
raihanromzi <raihan.rakhman16@gmail.com>
2023-07-06 06:23:10 +0700

    finish functional programming

---
+++ Self Study/dicoding/KotlinBasic/src/main/kotlin/FunctionType.kt
@@ -0,0 +1,10 @@
+fun main() {
+
+}
+
+// function type -> give data type to function
+typealias Arithmetic = (Int, Int) -> Int
+
+val sum: Arithmetic = { valueA, valueB -> valueA + valueB }
+
+val multiply: Arithmetic = { valueA, valueB -> valueA * valueB }
